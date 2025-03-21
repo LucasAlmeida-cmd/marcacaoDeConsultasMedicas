b7c25b4438dab1f4b8a821861950e04f1f762827
Lucas-CMD <lucasalmeidafmoraes@gmail.com>
2025-03-20 22:30:22 -0300

    Atualizando o app e index

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../screens/HomeScreen';
-import CreateAppointmentScreen from '../screens/CreateAppointmentScreen';
-import ProfileScreen from '../screens/ProfileScreen';
+import HomeScreen from '../src/screens/HomeScreen';
+import CreateAppointmentScreen from '../src/screens/CreateAppointmentScreen';
+import ProfileScreen from '../src/screens/ProfileScreen';
 
 const Stack = createNativeStackNavigator();
 
